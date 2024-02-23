abd2c51c61848f7edb7379c78c952cd864cbcc52
Alexander Kladov <alexander.a.kladov@gmail.com>
2024-02-23 15:54:14 +0400

    Add trailers covers config and fix apostrophe in url to info

--- make_movie_site.py
+++ make_movie_site.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
-import segno, os, sys
+import segno, os, sys, requests
 
-youtube_com = 'https://www.youtube.com/watch?v='
-wikipedia = 'https://en.wikipedia.org/wiki/'
-wiki_upload = 'https://upload.wikimedia.org/wikipedia/en/'
-movieposters = 'https://www.movieposters.com/collections/shop?q='
 autoplay = '&autoplay=1'
 style_data = "<style>body {background-color:#aaaadd;}</style>\n"
 info_data = ''
 info_dict=dict()
 html_data=dict()
+config=dict()
 distr=dict()
 year=""
+
+
+def download_poster(youtube_code):
+    global config
+    url = config['cover'] + youtube_code + '/maxresdefault.jpg'
+    local_f = "images/"+youtube_code+".png"
+    if not os.path.isfile(local_f):
+        r = requests.get(url)
+        f = open(local_f, 'wb')
+        for chunk in r.iter_content(chunk_size=512 * 1024): 
+            if chunk:
+                f.write(chunk)
+        f.close()
+    return local_f
+
+
+def load_config():
+    global config
+    movie_file = "movies_"
+    f = open("config.txt", "r")
+    if f:
+        line = f.readline().strip()
+        movie_file += line.split('#')[1]
+        movie_file += ".txt"
+        for line in f.readlines():
+            line = line.strip()
+            if len(line)>0 and line[0]!='#':
+                tokens = line.split('\t')
+                if tokens[0] not in config:
+                    config[tokens[0]]=tokens[1]
+    f.close()
+    return movie_file
+
 
 def read_distr(fname):
     global distr
@@ -27,10 +57,10 @@ def read_distr(fname):
 
 
 def qr_file_save(youtube_code):
-    global youtube_com
+    global config
     qrfile = "images/"+youtube_code+".png"
     if not os.path.isfile(qrfile):
-        qrcode = segno.make_qr(youtube_com + youtube_code)
+        qrcode = segno.make_qr(config['trailer'] + youtube_code)
         qrcode.save(qrfile, scale=5)
     return qrfile
 
@@ -57,11 +87,11 @@ def read_movies(fname, prefix):
             add_year(tokens[4])
             html_data[year] += "<tr>\n"
             html_data[year] += "<td>" + tokens[0] + "</td>\n"
-            html_data[year] += "<td><a href='"+movieposters+tokens[1].replace("'","&apos;").replace(' ','+')+"'><img src='" +wiki_upload + tokens[5]+"' width=160px height=200px></img></a></td>\n"
-            html_data[year] += "<td><a href='"+ wikipedia+tokens[3].replace("'","&apos;") +"'>" + tokens[1] + "</a></td>\n"
-            html_data[year] += "<td><a href='"+ wikipedia+distr[tokens[2]]+"'>"+ tokens[2]+"</a></td>\n"
-            qrfile = "../"+qr_file_save(tokens[6])
-            html_data[year] += "<td><a href='"+ youtube_com + tokens[6] +"'><img src='"+qrfile+"' width=200px height=200px></img></a></td>"
+            html_data[year] += "<td><a href='"+config['buy_poster']+tokens[1].replace("'","&apos;").replace(' ','+')+"'><img src='" + config['poster'] + tokens[5]+"' width=160px height=200px></img></a></td>\n"
+            html_data[year] += "<td><a href='"+ config['wiki']+tokens[3].replace("'","&apos;") +"'>" + tokens[1] + "</a></td>\n"
+            html_data[year] += "<td><a href='"+ config['wiki']+distr[tokens[2]]+"'>"+ tokens[2]+"</a></td>\n"
+            qrfile = "../"+download_poster(tokens[6])
+            html_data[year] += "<td><a href='"+ config['trailer'] + tokens[6] +"'><img src='"+qrfile+"' width=300px height=200px></img></a></td>\n"
             key = tokens[0]+tokens[4]
             if len(info_data)>0 and key in info_dict:
                 if len(info_dict[key].split("\t"))>1:
@@ -70,8 +100,8 @@ def read_movies(fname, prefix):
                 else:
                     link = info_dict[key]
                     rem  = ""
-                html_data[year] += "<td><a style=\"font-size:18px;\" href='"+info_data+link+"'>"+prefix+":"+link+"</a></td>"
-                html_data[year] += "<td><a style=\"font-size:18px;\">"+rem+"</td>"
+                html_data[year] += "<td><a style=\"font-size:18px;\" href='"+info_data+link.replace("'","&apos;")+"'>"+prefix+":"+link+"</a></td>"
+                html_data[year] += "<td><a style=\"font-size:18px;\">"+rem+"</td>\n"
             html_data[year] += "</tr>\n"
     f.close()
 
@@ -120,7 +150,9 @@ if __name__ == "__main__":
         info_data = sys.argv[1]
         read_info(fname)
         prefix = fname.split(".txt")[0]
+    movie_file_name = load_config()
     read_distr("distributors.txt")
-    read_movies("movies.txt", prefix)
+    read_movies(movie_file_name, prefix)
     save_html(prefix)
     save_index(prefix)
+
