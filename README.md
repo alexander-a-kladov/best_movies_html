96b4455e786a9e0ebc509f3bcad4eddc893c1ab2
Alexander Kladov <alexander.a.kladov@gmail.com>
2024-10-02 14:17:11 +0400

    Make script more general

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # best_movies_html
-Html pages with best movies
+Html pages generator with best movies
+
+
+1. Use ./get_wiki_info.py year, to get data from wikipedia about top 10 movies of the year save results into file year_movies.txt.
+  Then copy all information from several files into general file movies.txt, only for config 'en'
+
+2. When you have filled movies.txt use ./make_movie_site.py it will get additional parameters from config.txt.
+  It creates html-pages for each year from movies.txt, and directory html-pages with covers of all movies in year order
+
+3. You can also use ./make_movie_site.py kinopoisk.txt to create html-pages with kinopoisk.ru links where you can probably watch that film
+
+or ./make_movie_site.py info.txt to create html-pages with your comments and local paths to film files
+
--- make_movie_site.py
+++ make_movie_site.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import segno, os, sys, requests
-from bs4 import BeautifulSoup as bs
 
 style_data = "<style>body {background-color:#aaaadd;}</style>\n"
 info_data = ''
+info_data1 = ''
 info_dict=dict()
 html_data=dict()
 index_img_dict=dict()
@@ -45,7 +45,7 @@ def read_distr(fname):
     f.close()
 
 
-def add_year(new_year):
+def add_year(prefix, new_year):
     global year, html_data, index_img_dict
     if year != new_year:
         if year != "":
@@ -56,13 +56,13 @@ def add_year(new_year):
             year = new_year
             html_data[year] = "<html>\n"+style_data+"<body>\n"
             html_data[year] += "<div style=\"font-size:42px;\">"
-            html_data[year] += "<a href='../index.html'><img src=../images/film.png height=50px></a>"
+            html_data[year] += "<a href='../"+prefix+".html'><img src=../images/film.png height=50px></a>"
             html_data[year] += "<a href='"+config['wiki']+year+"_in_film'>"+year+"</a></div>"
             html_data[year] += "<table style=\"font-size:42px;\">\n"
 
 
 def read_movies(fname, prefix):
-    global html_data, youtube_com, wikipedia, distr
+    global html_data, youtube_com, wikipedia, distr, info_data, info_data1
     f = open(fname, "r")
     if f:
         for line in f.readlines():
@@ -72,7 +72,7 @@ def read_movies(fname, prefix):
             if len(tokens)<6:
                 continue
             #print(tokens)
-            add_year(tokens[4])    
+            add_year(prefix, tokens[4])    
             key = tokens[0]+tokens[4]
             link = ""
             rem  = ""
@@ -92,8 +92,8 @@ def read_movies(fname, prefix):
             if len(link)>0:
                 html_data[year] += "<td><a href='"+info_data+link.replace("'","&apos;")+"'><img src='../images/play.png' width=100px height=100px></a>\n"
             else:
-                if info_data.find('dl.php?t=') != -1:
-                    html_data[year] += "<td><a href='"+info_data.replace('dl.php?t=', 'tracker.php?nm=')+tokens[1]+ "' target=_blank><img src='../images/search.png' width=100px height=100px></a>\n"
+                if info_data1:
+                    html_data[year] += "<td><a href='"+info_data1+tokens[1]+ "' target=_blank><img src='../images/search.png' width=100px height=100px></a>\n"
                 else:
                     html_data[year] += "<td>\n"
             html_data[year] += "<a href='"+ config['wiki']+tokens[3].replace("'","&apos;") +"'>" + tokens[1] + "</a></td>\n"
@@ -114,8 +114,15 @@ def read_movies(fname, prefix):
 
 
 def read_info(fname):
+    global info_data, info_data1
     f = open(fname, "r")
     if f:
+        line = f.readline()
+        if line[0]=='#':
+            tokens = line[1:].split('\t')
+            info_data = tokens[0]
+            if len(tokens)==2:
+                info_data1 = tokens[1]
         for line in f.readlines():
             tokens = line.strip().split('\t')
             if len(tokens)<3:
@@ -131,7 +138,7 @@ def read_info(fname):
 def save_index(prefix):
     global html_data, index_img_dict
     index_html = "<html>\n<body style=\"font-size:42px;background-color:#dddd88;\">\n"
-    index_html += "<p><a href='index.html'><img src=images/film.png height=50px></a>The Most Successful Movies of the Year</p>"
+    index_html += "<p><a href='"+prefix+".html'><img src=images/film.png height=50px></a>The Most Successful Movies of the Year</p>"
     for year_key in html_data.keys():
         index_html += "<div><a href='"+"pages/"+prefix+year_key+".html'>"
         for img_index in range(len(index_img_dict[year_key])//2):
@@ -158,9 +165,8 @@ def save_html(prefix):
 
 if __name__ == "__main__":
     prefix = "index"
-    if len(sys.argv) > 2:
-        fname = sys.argv[2]
-        info_data = sys.argv[1]
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
         read_info(fname)
         prefix = fname.split(".txt")[0]
     movie_file_name = load_config()
